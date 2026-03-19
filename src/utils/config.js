ab820ae12b920a957c4b7e4a3cff5450ac511edf
Gabriela Castro <38534250+gabrielacastroh@users.noreply.github.com>
2026-03-19 13:47:59 -0500

    Update API URL in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,4 +3,4 @@
  * Se obtiene de la variable de entorno VITE_API_URL para poder cambiar por entorno.
  * En build: definir VITE_API_URL (ej. https://api.tudominio.com).
  */
-export const api = 'https://18.223.117.138.nip.io';
+export const api = 'https://18.223.117.138';
